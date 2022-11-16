a7f0a2b0cfe527899cc7046da740e1b521b40011
littlekingdom <wingindream@gmail.com>
2022-11-16 19:02:38 +0900

    aidResult 에 aidKey 추가

--- src/main/kotlin/kr/co/postbox/dto/aid/AidResultDTO.kt
+++ src/main/kotlin/kr/co/postbox/dto/aid/AidResultDTO.kt
@@ -4,11 +4,14 @@ import kr.co.postbox.dto.member.MemberResultDTO
 import kr.co.postbox.entity.aid.TbAid
 
 data class AidResultDTO(
-        var member:MemberResultDTO
+        var member:MemberResultDTO,
+        var aidKey:Long?
     )
 {
     constructor(tbAid:TbAid):this(
-    member = MemberResultDTO(tbAid.member)
+    member = MemberResultDTO(tbAid.member),
+    aidKey = tbAid.aidKey
+
     )
 }
 
